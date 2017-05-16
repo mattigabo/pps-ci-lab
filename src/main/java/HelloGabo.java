7840867ed6782dce71ebeb3058c8625dff3e8f31
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2017-05-16 16:10:31 +0200

    add a basic java class

---
+++ src/main/java/HelloGabo.java
@@ -0,0 +1,10 @@
+package main.java;
+
+/**
+ * Created by Matteo Gabellini on 16/05/2017.
+ */
+public class HelloGabo {
+    public static void main(String[] args) {
+        System.out.println("Hello Teams by The Project Leader");
+    }
+}
